5810bfc6c33d8f15586e9169531c00ce43cee4fa
Alen <sulejmanovic.alen5@gmail.com>
2023-01-18 18:40:00 +0100

    git commit -m "commitane vaje"

---
+++ LamdaIzrazi/LamdaIzrazi/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LamdaIzrazi
+{
+    internal class Program
+    {
+        //delegate bool FunkcijaZaNize(String s);
+
+        static void Main(string[] args)
+        {
+
+            string[] imena = { "David", "Blaž", "Ivan", "Neža", "Alen", "Saša", "Erik", "Simon", "Mitja", "Kristjan" };
+            List<string> a = DelajOperacijeNadNizi(imena, ZacneSS);
+
+            //List<string> b = DelajOperacijeNadNizi(imena, delegate (string s) { return s.EndsWith("n"); });
+            List<string> b = DelajOperacijeNadNizi(imena, s=> s.EndsWith("n"));
+            foreach (string x in b)
+            {
+                Console.WriteLine(x);
+            }
+            //List<string> naS = new List<string>();
+            //foreach (var x in imena)
+            //{
+            //    if (x.StartsWith("S"))
+            //    {
+            //        naS.Add(x);
+            //        Console.WriteLine(x);
+            //    }
+
+            //}
+            Console.ReadLine();
+        }
+        static List<string> DelajOperacijeNadNizi(string[] mojiNizi, Func<string,bool> mojaFunkcija)
+        {
+            List<string> rezultat = new List<string>();
+            foreach(string x in mojiNizi)
+            {
+                if (mojaFunkcija(x))
+                    rezultat.Add(x);
+            }
+            return rezultat;
+        }
+        static bool ZacneSS(string a)
+        {
+            return a.StartsWith("S");
+
+        }
+        static bool KoncaZN(string a)
+        {
+            return a.EndsWith("n");
+        }
+    }
+}
---
+++ VajaFuncDelegate/VajaFuncDelegate/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VajaFuncDelegate
+{
+    internal class Program
+    {
+        delegate string Spremeni(string s);
+        static void Main(string[] args)
+        {
+            Func<string, string> metoda = x => x.ToUpper();
+            string ime = "Barbara";
+            Console.WriteLine(metoda(ime));
+            Console.ReadLine();
+        }
+
+        //private static string VVelike(string s)
+        //{
+        //    return s.ToUpper();
+        //}
+    }
+}
